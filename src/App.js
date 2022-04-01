96a753f02641379cf6fa52688574b8cb9d7cb494
Gourav1221 <gouravmisra111@gmail.com>
2022-04-01 18:40:26 +0530

    right sidebar done needs to chagne css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.scss";
 import { Content } from "./Components/Content";
+import { RightSidebar } from "./Components/RightSidebar";
+
 function App() {
   return (
     <div className="App">
+     
       <Content />
+      <RightSidebar/>
     </div>
   );
 }
