fba4de4a7c455f976c894d30ed56679ce7e14841
Kamlesh Jadhav <kamleshjadhav4393@gmail.com>
2022-04-01 14:23:45 +0530

    updated

---
+++ src/Components/Video.jsx
@@ -0,0 +1,53 @@
+
+import "../Styles/Video"
+import { useEffect, useState } from "react";
+
+
+export const VideoSection = () => {
+
+    const [user, setUser] = useState([])
+    
+    const fetchdata = () =>{
+        fetch("https://dev.to/api/videos")
+        .then((res) =>{
+            return res.json();
+            // console.log(res)
+        }).then((data) =>{
+            setUser(data)
+            console.log(data)
+        })
+    }
+
+    useEffect( () =>{
+        fetchdata();
+    },[])
+
+  
+
+return (
+    
+  <div className="MainDiv_Videos">  
+  
+      
+      {
+          user.map(data =>(
+             
+            <div className="insideDiv_Videos" key={data.id}>
+
+                <img src={data.cloudinary_video_url} alt="" /> 
+                {/* <iframe src={data.video_source_url} frameborder="0"></iframe> */}
+
+                <div className="insideDiv_Videos_Durantion"> 
+                    <p>{data.video_duration_in_minutes}</p>
+                </div>
+
+                <p>{data.title}</p>
+
+            </div>
+
+          ))
+      }
+  </div>
+);
+
+};
